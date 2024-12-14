f85569a8e856c0d9073bed1651e5f904c06f787d
mfurqon <furqon.mfp@gmail.com>
2024-12-14 14:00:15 +0700

    chore: mengubah konfigurasi session
    - mengubah konfigurasi nama session di file config.php

--- application/config/config.php
+++ application/config/config.php
@@ -388,7 +388,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'ci_session_klinik_2';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
