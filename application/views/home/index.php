aeb56013dd7388535a77fab8068bf2af972e3dd6
mfurqon <furqon.mfp@gmail.com>
2024-12-16 17:55:14 +0700

    refactor: memindahkan kode sweet alert
    - mengubah struktur kode sweet alert dengan memindahkannya ke templates/header.php agar lebih efisien

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,20 +1,6 @@
             <!-- Start Header -->
             <div class="container-fluid header bg-primary p-0 mb-5">
-
-                <?php if ($this->session->flashdata('pesan')) : ?>
-                    <script>
-                        Swal.fire({
-                            title: 'Berhasil!',
-                            text: '<?= $this->session->flashdata('pesan') ?>',
-                            icon: 'success',
-                            confirmButtonText: 'OK',
-                            customClass: {
-                                confirmButton: 'btn btn-primary'
-                            }
-                        });
-                    </script>
-                <?php endif; ?>
-
+                
                 <div class="row g-0 align-items-center flex-column-reverse flex-lg-row">
                     <div class="col-lg-6 p-5 wow fadeIn" data-wow-delay="0.1s">
                         <h1 class="display-4 text-white mb-5">
