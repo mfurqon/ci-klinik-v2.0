0278972237a204844b8bee12a3964e99f9cc4e14
mfurqon <furqon.mfp@gmail.com>
2024-12-17 16:38:02 +0700

    chore: mengubah konfigurasi autoload helper
    - mengubah konigurasi autoload helper dengan menambahkan form_validation_helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'security', 'klinik');
+$autoload['helper'] = array('url', 'file', 'security', 'klinik', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
