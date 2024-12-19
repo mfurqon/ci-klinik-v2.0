bffa49459b29bd6ffe924c1324d9d794a59e0dba
mfurqon <furqon.mfp@gmail.com>
2024-12-20 02:10:57 +0700

    refactor: mengubah nama models
    - mengubah nama models di config/autoload yang tadinya ModelX -> ModelX

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('ModelObat', 'ModelDokter', 'ModelJanjiTemu', 'ModelUser');
+$autoload['model'] = array('ObatModel', 'DokterModel', 'JanjiTemuModel', 'UserModel');
