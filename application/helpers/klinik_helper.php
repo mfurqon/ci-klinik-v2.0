f266ca113f1e73a53ea98477996ddb0ca85b4aff
mfurqon <furqon.mfp@gmail.com>
2024-12-23 19:59:06 +0700

    refactor: mengubah nama function di helper
     - mengubah beberapa uri di function redirect
     - mengubah beberapa nama function di klinik_helper agar lebih jelas
     - menambahkan suffix admin atau user di penamaan function klinik_helper
     - suffix admin mengartikan function tersebut untuk admin
     - suffix user mengartikan function tersebut untuk user
     - menambahkan beberapa komentar untuk dokumentasi
     - menghapus baris kode tidak penting di function cek_belum_login_admin

--- application/helpers/klinik_helper.php
+++ application/helpers/klinik_helper.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No Direct Script Access Allowed');
 
-function cek_login()
+// function untuk admin
+// jika admin belum login, maka admin akan di-redirect ke halaman login admin
+function cek_belum_login_admin()
 {
     $ci = get_instance();
 
@@ -12,35 +14,12 @@ function cek_login()
                 Akses ditolak. Anda belum login!!
             </div>'
         );
-        if ($ci->session->userdata('role_id') == 1) {
-            redirect('autentifikasi');
-        } else {
-            redirect('home');
-        }
-    } else {
-        $role_id = $ci->session->userdata('role_id');
-        $id_user = $ci->session->userdata('id_user');
-    }
-}
-
-// jika user belum login, maka user akan di-redirect ke halaman login
-function cek_belum_login()
-{
-    $ci = get_instance();
-
-    if(!$ci->session->userdata('email')) {
-        $ci->session->set_flashdata(
-            'pesan',
-            '<div class="alert alert-danger" role="alert">
-                Anda belum login!!
-            </div>'
-        );
-        redirect('member');
-    }
+        redirect('admin/auth');
+    } 
 }
 
 // jika user sudah login, maka halaman login dan registrasi akan diblok
-function cek_sudah_login()
+function cek_sudah_login_admin()
 {
     $ci = get_instance();
 
@@ -51,12 +30,41 @@ function cek_sudah_login()
                 Anda sudah login!!
             </div>'
         );
-        redirect('user');
+        redirect('admin/dashboard');
+    }
+}
+
+// jika role_id user bukan administrator, maka user akan di-redirect ke halaman home
+function cek_akses()
+{
+    $ci = get_instance();
+
+    $role_id = $ci->session->userdata('role_id');
+    if ($role_id != 1) {
+        redirect('auth/blok');
+    }
+}
+
+
+// function untuk user
+// jika user belum login, maka user akan di-redirect ke halaman login user
+function cek_belum_login_user()
+{
+    $ci = get_instance();
+
+    if(!$ci->session->userdata('email')) {
+        $ci->session->set_flashdata(
+            'pesan',
+            '<div class="alert alert-danger" role="alert">
+                Anda belum login!!
+            </div>'
+        );
+        redirect('auth');
     }
 }
 
 // jika user sudah login, maka halaman login dan daftar akan diblok
-function cek_sudah_login_member()
+function cek_sudah_login_user()
 {
     $ci = get_instance();
 
@@ -68,16 +76,5 @@ function cek_sudah_login_member()
             </div>'
         );
         redirect('home');
-    }
-}
-
-// jika role_id user bukan administrator, maka user akan di-redirect ke halaman home
-function cek_akses()
-{
-    $ci = get_instance();
-
-    $role_id = $ci->session->userdata('role_id');
-    if ($role_id != 1) {
-        redirect('member/blok');
     }
 }
