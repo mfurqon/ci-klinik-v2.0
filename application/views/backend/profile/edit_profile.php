a7ee77c076ed3e44c7447e4e05ea8e58351bacbd
mfurqon <furqon.mfp@gmail.com>
2024-12-26 00:36:26 +0700

    feat: menambahkan pesan flashdata
    - menambahkan pesan flashdata
    - menambahkan beberapa style pada profile image
    - menambahkan tombol batal

--- application/views/backend/profile/edit_profile.php
+++ application/views/backend/profile/edit_profile.php
@@ -1,6 +1,10 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
+    <?php if ($this->session->flashdata('pesan')) : ?>
+        <?= $this->session->flashdata('pesan'); ?>
+    <?php endif; ?>
+
     <div class="row">
         <div class="col-lg-9">
 
@@ -20,7 +24,7 @@
                     <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
             </div>
-            
+
             <div class="form-group row">
                 <label for="telepon" class="col-sm-2 col-form-label">Nomor Telepon</label>
                 <div class="col-sm-10">
@@ -42,13 +46,14 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?= base_url('assets/img/profile/') . $user['gambar']; ?>" class="img-thumbnail">
+                            <img src="<?= base_url('assets/img/profile/') . $user['gambar']; ?>" class="img-thumbnail" style="width: 150px; height: 150px; object-fit: cover;">
                         </div>
                         <div class="col-sm-9">
                             <div class="custom-file">
                                 <input type="file" class="custom-file-input mb-3" id="gambar" name="gambar">
                                 <label class="custom-file-label" for="gambar">Pilih Gambar</label>
                                 <button type="submit" class="btn btn-primary">Ubah</button>
+                                <button type="button" class="btn btn-outline-primary" onclick="window.history.go(-1)">Batal</button>
                             </div>
                         </div>
                     </div>
