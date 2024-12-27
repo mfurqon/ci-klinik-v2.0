bbbb3f0f2e96eda180830baf58e514c1159adfe8
mfurqon <furqon.mfp@gmail.com>
2024-12-28 00:15:12 +0700

    refactor: mengubah fungsi sweet alert
    - mengubah fungsi sweet alert agar bisa reusable

--- assets/js/sweetalert.js
+++ assets/js/sweetalert.js
@@ -1,24 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    const button = document.getElementById("buat-janji-temu");
+console.log("Sweetalert.js Loaded");
 
-    if (form && button) {
-        button.addEventListener("click", function (event) {
-            event.preventDefault(); // Mencegah pengiriman form langsung
-            Swal.fire({
-                title: "Apakah Anda yakin?",
-                text: "Apakah Anda yakin ingin buat janji temu?",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Ya, buat!",
-                cancelButtonText: "Batal",
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    form.submit(); // Kirim form jika pengguna menekan "Ya"
-                }
-            });
-        });
-    }
-});
+function showConfirmAlert(title, text, url) {
+    console.log("Function Loaded");
+
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Ya, Lanjutkan',
+        cancelButtonText: 'Batal',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = url;
+        }
+    });
+}
