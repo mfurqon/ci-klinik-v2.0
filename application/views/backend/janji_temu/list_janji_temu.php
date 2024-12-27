f2e26d8823c8de9ca4c2642e2075c177a862aba6
mfurqon <furqon.mfp@gmail.com>
2024-12-27 23:53:08 +0700

    refactor: mengubah warna tombol
    - mengubah warna tombol ubah status
    - menambahkan field status pada table data janji temu

--- application/views/backend/janji_temu/list_janji_temu.php
+++ application/views/backend/janji_temu/list_janji_temu.php
@@ -27,6 +27,7 @@
                                     <th>Tanggal Temu</th>
                                     <th>Jam Temu</th>
                                     <th>Tanggal Dibuat</th>
+                                    <th>Status</th>
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
@@ -39,14 +40,15 @@
                                         <td><?= date('d-m-Y', strtotime($jt['tanggal_temu'])); ?></td>
                                         <td><?= $jt['jam_temu']; ?></td>
                                         <td><?= date('d-m-Y', strtotime($jt['tanggal_dibuat'])); ?></td>
+                                        <td><?= $jt['status']; ?></td>
                                         <td>
                                             <?php if ($jt['status'] == "Diproses") : ?>
-                                                <a class="btn btn-sm btn-outline-secondary">
+                                                <a href="<?= base_url('admin/janji-temu/ubah-status/' . $jt['id']); ?>" class="btn btn-sm btn-outline-primary">
                                                     <i class="fas fa-fw fa-edit"></i>
                                                     Ubah Status
                                                 </a>
                                             <?php else : ?>
-                                                <a href="<?= base_url('admin/janji-temu/ubah-status/' . $jt['id']); ?>" class="btn btn-sm btn-outline-primary">
+                                                <a class="btn btn-sm btn-outline-secondary">
                                                     <i class="fas fa-fw fa-edit"></i>
                                                     Ubah Status
                                                 </a>
