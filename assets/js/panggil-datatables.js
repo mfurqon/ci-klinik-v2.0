b71f07e076191ede9b04a10777bfa5e5a4c9a20c
mfurqon <furqon.mfp@gmail.com>
2024-12-28 00:24:08 +0700

    refactor: mengubah isi file
    - mengubah isi file panggil-datatables.js
    - mengubah id table

--- assets/js/panggil-datatables.js
+++ assets/js/panggil-datatables.js
@@ -1,11 +1,13 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-// window.$ = window.jquery = require('./node_modules/jquery');
-// window.dt = require('../../vendor/DataTablesKlinik/datatables.min.js')();
-// window.$('#dataTablesKlinik').DataTable();
-
 // Call the dataTables jQuery plugin
-$(document).ready(function () {
-	$("#dataTablesKlinik").DataTable();
+$(document).ready(function() {
+	if ($.fn.DataTable.isDataTable('#datatables-klinik')) {
+        $('#datatables-klinik').DataTable().clear().destroy();
+    }
+    $('#datatables-klinik').DataTable({
+		"paging": true,
+		"searching": true,
+		"info": true,
+		"ordering": true,
+		"autowidth": false,
+	});
 });
