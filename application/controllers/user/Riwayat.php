978dbdc3b5236c7e84642053caec89b333871b62
mfurqon <furqon.mfp@gmail.com>
2024-12-27 23:49:47 +0700

    feat: menambahkan beberapa method di controller Riwayat
    - menambahkan beberapa method di controller Riwayat
    - menambahkan method batalkanJanjiTemu agar user dapat membatalkan janji temu yang sudah dibuat
    - menambahkan function updateJanjiTemuOtomatis di method Riwayat/janjiTemu

--- application/controllers/user/Riwayat.php
+++ application/controllers/user/Riwayat.php
@@ -7,11 +7,49 @@ class Riwayat extends CI_Controller
     {
         $data['judul'] = 'Riwayat Janji Temu';
         $data['user'] = $this->UserModel->getUserWhere(['email' => $this->session->userdata('email')]);
-        $data['janji_temu'] = $this->JanjiTemuModel->getJanjiTemuById($data['user']['id']);
         $data['data_keranjang'] = $this->TempPemesananObatModel->getDataWhere(['id_user' => $this->session->userdata('id_user')]);
+
+        $this->JanjiTemuModel->updateJanjiTemuOtomatis();
+
+        $data['janji_temu'] = $this->JanjiTemuModel->getJanjiTemuById($data['user']['id']);
 
         $this->load->view('frontend/templates/main/header', $data);
         $this->load->view('frontend/riwayat/riwayat_janji_temu', $data);
         $this->load->view('frontend/templates/main/footer');
+    }
+
+    public function batalkanJanjiTemu()
+    {
+        $id_janji_temu = $this->uri->segment(3);
+
+        // Validasi ID janji temu
+        if (!is_numeric($id_janji_temu) || empty($id_janji_temu)) {
+            $this->session->set_flashdata('pesan', [
+                'title' => 'Gagal',
+                'text' => 'ID tidak valid!',
+                'icon' => 'error'
+            ]);
+
+            redirect('riwayat/janji-temu');
+            return;
+        }
+
+        $update = $this->JanjiTemuModel->cancelJanjiTemu($id_janji_temu);
+
+        if ($update) {
+            $this->session->set_flashdata('pesan', [
+                'title' => 'Berhasil',
+                'text' => 'Janji Temu berhasil dibatalkan!',
+                'icon' => 'success'
+            ]);
+        } else {
+            $this->session->set_flashdata('pesan', [
+                'title' => 'Gagal',
+                'text' => 'Gagal membatalkan Janji Temu',
+                'icon' => 'error'
+            ]);
+        }
+        
+        redirect('riwayat/janji-temu');
     }
 }
