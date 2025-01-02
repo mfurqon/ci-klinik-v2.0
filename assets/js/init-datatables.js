3a4dd821cf068e3c33b702bbad9db65c87d19e3f
mfurqon <furqon.mfp@gmail.com>
2025-01-02 15:27:38 +0700

    refactor: mengubah isi dari init-datatables
    - mengubah isi dari file init-datatables dengan menghapus beberapa baris dan menambahkan console.log agar mudah saat debugging

--- assets/js/init-datatables.js
+++ assets/js/init-datatables.js
@@ -1,13 +1,5 @@
-// Call the dataTables jQuery plugin
 $(document).ready(function() {
-	if ($.fn.DataTable.isDataTable('#datatables-klinik')) {
-        $('#datatables-klinik').DataTable().clear().destroy();
-    }
-    $('#datatables-klinik').DataTable({
-		"paging": true,
-		"searching": true,
-		"info": true,
-		"ordering": true,
-		"autowidth": false,
-	});
+	console.log('Inisialisasi DataTables dimulai');
+    $('#datatables-klinik').DataTable();
+	console.log('Inisialisasi DataTables selesai');
 });
