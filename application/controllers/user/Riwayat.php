d87451e6d3a5d0c676facd13fbd3d925b6b76478
mfurqon <furqon.mfp@gmail.com>
2025-01-02 16:45:52 +0700

    feat: menambahkan method baru di controller Riwayat
    - menambahkan method pemesananObat, printInvoice, exportPdfInvoice

--- application/controllers/user/Riwayat.php
+++ application/controllers/user/Riwayat.php
@@ -52,4 +52,54 @@ class Riwayat extends CI_Controller
         
         redirect('riwayat/janji-temu');
     }
+
+    public function pemesananObat()
+    {
+        $id_user = $this->session->userdata('id_user');
+
+        $data['judul'] = 'Riwayat Pemesanan Obat';
+        $data['user'] = $this->UserModel->getUserWhere(['email' => $this->session->userdata('email')]);
+        $data['data_keranjang'] = $this->TempPemesananObatModel->getDataPemesananObatWhere(['id_user' => $this->session->userdata('id_user')]);
+        $data['pemesanan_obat'] = $this->PemesananObatModel->getPemesananByIdUser($id_user);
+
+        $this->load->view('frontend/templates/main/header', $data);
+        $this->load->view('frontend/riwayat/riwayat_pemesanan_obat', $data);
+        $this->load->view('frontend/templates/main/footer');
+    }
+
+    public function printInvoice($id_pemesanan)
+    {
+        $data['judul'] = 'Cetak Invoice Pemesanan Obat';
+        $data['user'] = $this->UserModel->getUserWhere(['email' => $this->session->userdata('email')]);
+        $data['faktur'] = $this->PemesananObatModel->getPemesananByIdPemesanan($id_pemesanan);
+        $data['detail_obat'] = $this->PemesananObatModel->getDetailPemesananByIdPemesanan($id_pemesanan);
+
+        if (!$id_pemesanan) {
+            redirect('checkout-obat');
+        } else {
+            $this->load->view('frontend/pemesanan_obat/print_faktur', $data);
+        }
+    }
+
+    public function exportPdfInvoice($id_pemesanan)
+    {
+        $data['judul'] = 'Cetak Invoice Pemesanan Obat';
+        $data['user'] = $this->UserModel->getUserWhere(['email' => $this->session->userdata('email')]);
+        $data['faktur'] = $this->PemesananObatModel->getPemesananByIdPemesanan($id_pemesanan);
+        $data['detail_obat'] = $this->PemesananObatModel->getDetailPemesananByIdPemesanan($id_pemesanan);
+
+        $this->load->view('frontend/pemesanan_obat/export_pdf_faktur', $data);
+
+        $paper_size = 'A4';
+        $orientation = 'portrait';
+        $html = $this->output->get_output();
+
+        $this->load->library('pdf');
+
+        if (!$id_pemesanan) {
+            redirect('checkout-obat');
+        } else {
+            $this->pdf->generate($html, "pdf-pemesanan-obat", $paper_size, $orientation);
+        }
+    }
 }
