4cd893339327fe40d9e7d811d38d5b35647f846e
mfurqon <furqon.mfp@gmail.com>
2025-01-02 15:04:27 +0700

    feat: menambahkan beberapa function di PemesananObatModel
    - menambahkan function getPemesananByIdUser untuk mendapatkan data pemesanan obat by id_user
    - menambahkan function getPemesananByIdPemesanan untuk mendapatkan data pemesanan obat by id_pemesanan
    - menambahkan function getDetailPemesananByIdPemesanan by id_pemesanan

--- application/models/PemesananObatModel.php
+++ application/models/PemesananObatModel.php
@@ -34,4 +34,37 @@ class PemesananObatModel extends CI_Model
         ];
         $this->db->insert('pemesanan_obat', $data);
     }
+
+    public function getPemesananByIdUser($id_user)
+    {
+        $this->db->select('po.*, f.*, u.*, p.*, DATE(po.tanggal_pemesanan) AS tanggal, TIME(po.tanggal_pemesanan) AS jam');
+        $this->db->from('pemesanan_obat po');
+        $this->db->join('faktur f', 'po.id_pemesanan = f.id_pemesanan', 'left');
+        $this->db->join('pembayaran p', 'po.id_pemesanan = p.id_pemesanan', 'left');
+        $this->db->join('user u', 'u.id = po.id_user');
+        $this->db->where('po.id_user', $id_user);
+
+        return $this->db->get()->result_array();
+    }
+
+    public function getPemesananByIdPemesanan($id_pemesanan)
+    {
+        $this->db->select('*');
+        $this->db->from('pemesanan_obat po');
+        $this->db->join('faktur f', 'po.id_pemesanan = f.id_pemesanan');
+        $this->db->join('pembayaran p', 'p.id_pemesanan = po.id_pemesanan');
+        $this->db->join('user u', 'u.id = f.id_user');
+        $this->db->where('po.id_pemesanan', $id_pemesanan);
+
+        return $this->db->get()->row_array();
+    }
+    
+    public function getDetailPemesananByIdPemesanan($id_pemesanan)
+    {
+        $this->db->select('dpo.nama_obat, dpo.jumlah_obat, dpo.harga_obat');
+        $this->db->from('detail_pemesanan_obat dpo');
+        $this->db->join('pemesanan_obat po', 'dpo.id_pemesanan = po.id_pemesanan');
+        $this->db->where('dpo.id_pemesanan', $id_pemesanan);
+        return $this->db->get()->result_array();
+    }
 }
